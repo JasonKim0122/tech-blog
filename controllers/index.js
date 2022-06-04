cd2673d34f0cc5650b58053e47562b88b0ed6ea4
JasonKim0122 <JasonKim0122@gmail.com>
2022-06-04 12:40:29 -0500

    started initial setup

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
+
+router.use('/api', apiRoutes);
+router.use('/api', homeRoutes);
+router.use('/api', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
---
+++ models/Comment.js
@@ -0,0 +1,3 @@
+const {all} = require('express/lib/application');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
